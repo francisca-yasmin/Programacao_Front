4ac36989c9190d77f638ad5caa6ab048b271e3c3
francisca-yasmin <yasmimfreitaz5@gmail.com>
2024-08-15 10:47:56 -0300

    variaveis

--- aula1.py
+++ aula1.py
@@ -1,2 +1,27 @@
-print("ola mundo!")
-print("minha primeira aula de back")
+"""PRINT EXIBE MENSAGENS NA TELA"""
+
+print(">>>>>>>>>Vamos fazer uma panqueca<<<<<<<<<")
+
+"""input serve para perguntar ao usuario"""
+cozinheiro = input("Quem é o cozinheiro de hoje? ")
+
+#ingredientes
+ovos = int(input("qual a quantidade de ovos necessaria? "))
+leite = float(input("quanto leite sera necessario? "))
+farinha = float(input("quanto de farinha sera necessario? "))
+
+"""cozinheiro = "fran"
+ovos = 2
+farinha = 0.5
+leite = 0.200"""
+
+"""print(type(cozinheiro))
+print(type(ovos))
+print(type(farinha))
+print(type(leite))"""
+
+print(f"ola {cozinheiro}, pronto para a nossa receita de hoje? ")
+print("na nossa receita")
+print(f"usaremos {ovos} ovos, {leite} xicaras de leite e {farinha} xicaras de farinha")
+
+
